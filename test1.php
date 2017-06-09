3d0f084aee6994e68b410e89aa6d3fd0a7fa19fa
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-06-09 14:00:46 -0600

    more testing

--- test1.php
+++ test1.php
@@ -9,7 +9,12 @@
 		echo $username;
 		echo "<br>";
 		$current_user = $username;
-		echo $current_user;
+		echo $current_user . "<br>";
+		$y = 0;
+		if ($y-- == 0) echo $y."<br>";
+		$count = 30;
+		echo 'this week the count is $count';
+		echo "<br>this week the count is $count";
 		?>
 
 	</body>
